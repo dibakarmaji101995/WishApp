84d86fe83032321da9dce21558fb7f8f1f70e8ca
DIBAKAR MAJI <39332908+dibakarmaji101995@users.noreply.github.com>
2019-04-08 01:53:23 +0530

    first commit

---
+++ src/main/java/com/nt/resource/WishMessageGeneratorResource.java
@@ -0,0 +1,18 @@
+package com.nt.resource;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
+
+@Path("/welcome")
+public class WishMessageGeneratorResource {
+	public WishMessageGeneratorResource() {
+		System.out.println("WishMessageGeneratorResource.WishMessageGeneratorResource()");
+	}
+	
+	//http://localhost:3030/JAX-RS-RESFUL-APP/rest/welcome
+	@GET
+    public String wishMessageGenerate() {
+    	return "Welcome to Restful services.....!!!!";
+    }//method
+}//class
